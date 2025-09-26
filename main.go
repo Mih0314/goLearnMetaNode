502e3a8968fdea4bf07b028874efb4affc950811
xumh <xumingh@postar.cn>
2025-09-26 21:26:08 +0800

    第一阶段作业

--- main.go
+++ main.go
@@ -2,12 +2,97 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
 	//singleNumber([]int{2, 2, 1})
 	//println(isValid("()[]{}"))
-	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
+	//fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
+	//fmt.Println(plusOne([]int{1, 2, 3}))
+	//fmt.Println(removeDuplicates([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}))
+	//fmt.Println(merge([][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}))
+}
+
+/*
+*
+
+	以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+*/
+func merge(intervals [][]int) [][]int {
+	if len(intervals) == 0 {
+		return nil
+	}
+
+	// 1. 按区间起始值排序
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	// 2. 初始化结果集
+	result := [][]int{intervals[0]}
+	fmt.Println(result)
+
+	// 3. 遍历合并
+	for _, interval := range intervals[1:] {
+		last := &result[len(result)-1] // 引用结果集最后一个区间
+		if interval[0] <= (*last)[1] {
+			// 重叠：更新结束值为较大者
+			if interval[1] > (*last)[1] {
+				(*last)[1] = interval[1]
+			}
+		} else {
+			// 不重叠叠：直接加入
+			result = append(result, interval)
+		}
+	}
+
+	return result
+}
+
+/*
+*
+
+	给你一个 非严格递增排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+	考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
+	更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要。
+	返回 k 。
+*/
+func removeDuplicates(nums []int) int {
+	m := make(map[int]int)
+	n := []int{}
+	for _, k := range nums {
+		_, exist := m[k]
+		if exist {
+			continue
+		} else {
+			n = append(n, k)
+			m[k] = 1
+		}
+	}
+	fmt.Println(n)
+	return len(n)
+}
+
+/*
+*
+给定一个表示 大整数 的整数数组 digits，其中 digits[i] 是整数的第 i 位数字。这些数字按从左到右，从最高位到最低位排列。这个大整数不包含任何前导 0。
+将大整数加 1，并返回结果的数字数组。
+*/
+func plusOne(digits []int) []int {
+	n := len(digits)
+
+	for i := n - 1; i >= 0; i-- {
+		digits[i]++
+
+		if digits[i] < 10 {
+			return digits
+		}
+
+		digits[i] = 0
+	}
+
+	return append([]int{1}, digits...)
 }
 
 /*
